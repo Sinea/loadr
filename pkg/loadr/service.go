18279ffc56de1e0e175ee25613e6bb889630458f
Sinea <bryan.fury.snt@gmail.com>
2019-04-02 20:22:05 +0300

    Better logging

--- pkg/loadr/service.go
+++ pkg/loadr/service.go
@@ -1,10 +1,10 @@
 package loadr
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -20,6 +20,7 @@ type service struct {
 	errors          chan error
 	cleanupInterval time.Duration
 	isCleaningUp    bool
+	logger          *log.Logger
 }
 
 func (s *service) Errors() <-chan error {
@@ -62,8 +63,8 @@ func (s *service) handleProgress(p MetaProgress) {
 	if clients, ok := s.clients[p.Token]; ok {
 		for _, client := range clients {
 			if err := client.WriteJSON(p.Progress); err != nil {
-				s.errors <- fmt.Errorf("error writing to client: %s", err)
-				closeWebSocket(client)
+				s.logger.Printf("error writing to client %s: %s\n", client.RemoteAddr(), err)
+				s.closeWebSocket(client)
 			}
 		}
 	}
@@ -75,15 +76,14 @@ func (s *service) cleanupClients() {
 		return
 	}
 	s.isCleaningUp = true
-	// TODO : Lock each token
 	for token, clients := range s.clients {
 		remaining := make([]*websocket.Conn, 0)
 		for _, c := range clients {
 			if err := c.SetReadDeadline(time.Now().Add(time.Millisecond)); err != nil {
-				log.Printf("error setting deadline %s", err)
+				s.logger.Printf("error setting deadline %s", err)
 			}
 			if _, _, err := c.ReadMessage(); err != nil && websocket.IsCloseError(err, websocket.CloseGoingAway) {
-				closeWebSocket(c)
+				s.closeWebSocket(c)
 			} else {
 				remaining = append(remaining, c)
 			}
@@ -98,15 +98,17 @@ func (s *service) wsHandler(c echo.Context) error {
 	ws, err := s.upgrader.Upgrade(c.Response(), c.Request(), nil)
 
 	if err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+		s.logger.Printf("error upgrading protocol: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	if progress, err := s.store.Get(token); err == nil {
 		if err := ws.WriteJSON(progress); err != nil {
-			s.errors <- fmt.Errorf("error writing initial progress state: %s", err)
-			return c.String(http.StatusInternalServerError, err.Error())
+			s.logger.Printf("error writing initial progress state: %s\n", err)
+			s.closeWebSocket(ws)
+			return c.NoContent(http.StatusInternalServerError)
 		}
 	} else {
-		s.errors <- fmt.Errorf("error retrieving initial progress state: %s", err)
+		s.logger.Printf("error retrieving initial progress state: %s\n", err)
 	}
 
 	s.clients[token] = append(s.clients[token], ws)
@@ -123,20 +125,19 @@ func (s *service) updateProgress(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 	if err := validator.Validate(update); err != nil {
-		return err
+		s.logger.Printf("error validating progress request: %s\n", err)
+		return c.NoContent(http.StatusBadRequest)
 	}
 	if err := s.store.Set(token, &update.Progress); err != nil {
-		err := fmt.Errorf("error saving progress: %s", err)
-		s.errors <- err
+		s.logger.Printf("error saving progress: %s\n", err)
 		if update.Guarantee >= Storage {
-			return c.String(http.StatusInternalServerError, err.Error())
+			return c.NoContent(http.StatusInternalServerError)
 		}
 	}
 	if err := s.channel.Push(MetaProgress{token, update.Progress}); err != nil {
-		err := fmt.Errorf("error broadcasting progress: %s", err)
-		s.errors <- err
+		s.logger.Printf("error broadcasting progress: %s\n", err)
 		if update.Guarantee >= Broadcast {
-			return c.String(http.StatusInternalServerError, err.Error())
+			return c.NoContent(http.StatusInternalServerError)
 		}
 	}
 	return c.NoContent(http.StatusOK)
@@ -147,25 +148,26 @@ func (s *service) deleteProgress(c echo.Context) error {
 	token := Token(tokenString)
 	if clients, ok := s.clients[token]; ok {
 		for _, client := range clients {
-			closeWebSocket(client)
+			s.closeWebSocket(client)
 		}
 	}
 	s.clients[token] = make([]*websocket.Conn, 0)
 	if err := s.store.Delete(token); err != nil {
-		s.errors <- fmt.Errorf("error deleting progress for token '%s' : %s", token, err)
+		s.logger.Printf("error deleting progress for token '%s' : %s\n", token, err)
 		return c.NoContent(http.StatusNotFound)
 	}
 	return c.NoContent(http.StatusOK)
 }
 
-func closeWebSocket(ws io.Closer) {
+func (s *service) closeWebSocket(ws io.Closer) {
 	if err := ws.Close(); err != nil {
-		log.Println("error closing client socket")
+		s.logger.Println("error closing client socket")
 	}
 }
 
 func New(store ProgressStore, channel Channel) Service {
 	return &service{
+		logger:          log.New(os.Stdout, "", 0),
 		cleanupInterval: time.Second * 3,
 		upgrader:        websocket.Upgrader{},
 		store:           store,
